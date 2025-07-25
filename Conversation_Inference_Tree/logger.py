bb7006e916b9e80f2fbe67fa4d2c5ad43b71ff70
BraydenAC <ba.cloutier@yahoo.com>
2025-07-25 12:43:13 -0400

    Updated logger to avoid file errors

--- Conversation_Inference_Tree/logger.py
+++ Conversation_Inference_Tree/logger.py
@@ -1,26 +1,32 @@
 import logging
 import time
+import os
 
-logger = logging.getLogger(__name__)
+# Time format for filenames
+t = time.localtime()
+current_time = time.strftime("%H-%M-%S", t)  # Avoid colon ":" in filenames (especially on Windows)
 
+# Directory setup
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG_DIR = os.path.join(BASE_DIR, '.logs')
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# File paths (use f-strings for actual variable substitution)
+LOG_FILE = os.path.join(LOG_DIR, f'log_{current_time}.log')
+PROGRESS_FILE = os.path.join(LOG_DIR, f'progress_{current_time}.log')
+
+# Logger for general debug/info
+logger = logging.getLogger('main_logger')
 logger.setLevel(logging.DEBUG)
 
-t = time.localtime()
-current_time = time.strftime("%H:%M:%S", t)
+file_handler = logging.FileHandler(LOG_FILE)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(file_handler)
 
-handler = logging.FileHandler(f"./.logs/log_{current_time}.txt")
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-###################################################################################
-
-log_progress = logging.getLogger(__name__)
-
+# Logger for progress (separate file)
+log_progress = logging.getLogger('progress_logger')
 log_progress.setLevel(logging.INFO)
 
-progress_handler = logging.FileHandler(f"./.logs/progress_{current_time}.txt")
-
-progress_handler.setFormatter(formatter)
+progress_handler = logging.FileHandler(PROGRESS_FILE)
+progress_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 log_progress.addHandler(progress_handler)
